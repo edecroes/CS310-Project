dcb994dca5cd4318ee42e4d81e2b5958ac578032
Wilhelm Gusztav M. Francia <wfrancia@stu.jsu.edu>
2018-03-07 12:05:52 -0600

    Established Connection to Server

--- Documents/NetBeansProjects/cs310-team-project/src/cs310/team/project/TASDatabase.java
+++ Documents/NetBeansProjects/cs310-team-project/src/cs310/team/project/TASDatabase.java
@@ -2,7 +2,22 @@ package cs310.team.project;
 import java.sql.*;
 
 public class TASDatabase {
-
-
+    Connection conn;
+    
+    public TASDatabase() throws SQLException{
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            
+            String url = "jdbc:mysql://localhost/tas";
+            
+            conn = DriverManager.getConnection(url,"cs310","spring2018");
+            
+        } 
+        
+        catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+    }
 
 }

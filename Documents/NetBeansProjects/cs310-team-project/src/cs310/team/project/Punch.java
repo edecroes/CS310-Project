47f467729f732848df2f8910466ce3a726d4e4c4
Wilhelm Gusztav M. Francia <wfrancia@stu.jsu.edu>
2018-03-12 12:14:08 -0500

    plus toString();

--- Documents/NetBeansProjects/cs310-team-project/src/cs310/team/project/Punch.java
+++ Documents/NetBeansProjects/cs310-team-project/src/cs310/team/project/Punch.java
@@ -1,5 +1,6 @@
 package cs310.team.project;
 import java.util.*;
+import java.text.*;
 
 public class Punch {
 private int id;
@@ -18,19 +19,19 @@ public Punch(int id, int terminalid, String badgeid, GregorianCalendar originalt
 	this.eventdata = eventdata;
 }
 
-public String getid() {
+public int getid() {
 	return this.id;
 }
 
-public void setid(String id) {
+public void setid(int id) {
 	this.id = id;
 }
 
-public String getterminalid() {
+public int getterminalid() {
 	return this.terminalid;
 }
 
-public void setterminalid(String terminalid) {
+public void setterminalid(int terminalid) {
 	this.terminalid = terminalid;
 }
 
@@ -42,11 +43,11 @@ public void setbadgeid(String badgeid) {
 	this.badgeid = badgeid;
 }
 
-public String getoriginaltimestamp() {
+public GregorianCalendar getoriginaltimestamp() {
 	return this.originaltimestamp;
 }
 
-public void setoriginaltimestamp(String originaltimestamp) {
+public void setoriginaltimestamp(GregorianCalendar originaltimestamp) {
 	this.originaltimestamp = originaltimestamp;
 }
 
@@ -66,6 +67,14 @@ public void seteventdata(Object eventdata) {
 	this.eventdata = eventdata;
 }
 
+public String printOriginalTimestamp() {
+    //"#D2C39273 CLOCKED IN: WED 09/06/2017 07:00:07"
+
+    //format string: "EEE "
+    SimpleDateFormat s = new SimpleDateFormat("EEE ");      
+    return "#"+this.id+" CLOCKED IN "+s.format()+ 
+    
+}
 
 
 
